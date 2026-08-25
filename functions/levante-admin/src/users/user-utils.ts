c46d23df97ca740f164207078b836c29daa8f243
u2234-io <alex@u2234.io>
2026-08-25 11:52:39 -0400

    feat: getUsersByOrg callable (#123)

--- functions/levante-admin/src/users/user-utils.ts
+++ functions/levante-admin/src/users/user-utils.ts
@@ -4,3 +4,10 @@ export const LEVANTE_TO_ROAR_USERTYPE = {
   child: "student",
   teacher: "teacher",
 } as const;
+
+export const ROAR_TO_LEVANTE_USERTYPE = {
+  admin: "admin",
+  parent: "caregiver",
+  student: "child",
+  teacher: "teacher",
+} as const;
